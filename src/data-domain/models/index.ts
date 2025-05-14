fe4247fb9cc868fb114848e34de828c8f21c5eea
Lukas Mundt <mail@lukas-mundt.de>
2025-05-14 23:32:08 +0200

    [SE2-6] Add models

---
+++ src/data-domain/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Category.ts';
+export * from './Entry.ts';
+export * from './Game.ts';
+export * from './Leaderboard.ts';
+export * from './ModelError.ts';
+export * from './Runtime.ts';
