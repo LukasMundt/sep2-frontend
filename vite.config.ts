1b21712c132a15c3360d6be276c2ae0fb8588d01
Lukas Mundt <mail@lukas-mundt.de>
2025-05-15 00:10:38 +0200

    [SE2-6] temporary proxy

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://speedrun-backend-staging.lukas-mundt.de/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
