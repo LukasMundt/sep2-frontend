8c3b363757b63c5fda7f71ad4e46d8f1b121035d
Lukas Mundt <mail@lukas-mundt.de>
2025-05-15 22:27:32 +0200

    [SE2-6] modify business rules to use new api client

--- src/business-rules/get-leaderboard-from-game-and-category.ts
+++ src/business-rules/get-leaderboard-from-game-and-category.ts
@@ -1,18 +1,24 @@
-import {Category, Leaderboard} from "@/data-domain";
-import {ErrorModel} from "@/data-domain/clients/error.model.ts";
-import GetLeaderboardGameCategoryClient
-    from "@/data-domain/clients/get-leaderboard-game-category/get-leaderboard-game-category.client.ts";
+import createClient from "openapi-fetch";
+import type {paths} from "@/data-domain/schema";
 
 export default async function GetLeaderboardFromGameAndCategory(
-    game: string|undefined,
-    category: Category|undefined,
-): Promise<ErrorModel | Leaderboard> {
-    if(game == undefined)
-        return {message: "game can't be undefined"}
-    if(category == undefined)
-        return {message: "category can't be undefined"}
+    gameSlug: paths["/rest/api/games/{gameSlug}/{categoryId}/leaderboard"]["get"]["parameters"]["path"]["gameSlug"],
+    category: paths["/rest/api/games/{gameSlug}/{categoryId}/leaderboard"]["get"]["parameters"]["path"]["categoryId"] ,
+) {
+    const client = createClient<paths>()
+    const {data, error} = await client.GET(
+        "/rest/api/games/{gameSlug}/{categoryId}/leaderboard", {
+            params: {
+                path: {
+                    gameSlug: gameSlug,
+                    categoryId: category
+                }
+            }
+        }
+    );
 
-    const client = new GetLeaderboardGameCategoryClient()
-    return await client.fetch(game, category);
-
+    return {
+        data: data,
+        error: error
+    };
 }
