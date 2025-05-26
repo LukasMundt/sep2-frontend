dc6c99ef02894b7ac41735f5a7d0961bcd9054c7
Lukas Mundt <mail@lukas-mundt.de>
2025-05-26 19:07:16 +0200

    [SE2-34] add form to upload speedrun

--- src/presentation/pages/upload-speedrun/UploadSpeedrun.tsx
+++ src/presentation/pages/upload-speedrun/UploadSpeedrun.tsx
@@ -1,3 +1,136 @@
+import {useSearchParams} from "react-router-dom";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useEffect, useState} from "react";
+import GetGames from "@/business-rules/get-games.ts";
+import {toast} from "sonner";
+import {components} from "@/data-domain/schema";
+import GetCategoriesFromGame from "@/business-rules/get-categories-from-game.ts";
+import {Card, CardContent, CardDescription, CardHeader} from "@/presentation/components/ui/card.tsx";
+import {Form} from "@/presentation/components/ui/form.tsx";
+import {Button} from "@/presentation/components/ui/button";
+
+import FormFieldGame from "@/presentation/pages/upload-speedrun/components/FormFieldGame.tsx";
+import FormFieldCategory from "@/presentation/pages/upload-speedrun/components/FormFieldCategory.tsx";
+import FormFieldDate from "@/presentation/pages/upload-speedrun/components/FormFieldDate.tsx";
+import FormFieldRuntime from "@/presentation/pages/upload-speedrun/components/FormFieldRuntime.tsx";
+
 export default function UploadSpeedrun() {
-    return (<div>UploadSpeedrun</div>);
+    const [searchParams] = useSearchParams();
+
+    const [games, setGames] = useState<components["schemas"]["GameDto"][] | undefined>(undefined);
+    const [gamesLoading, setGamesLoading] = useState<boolean>(false);
+    const [gamesError, setGamesError] = useState<undefined | boolean>(undefined);
+
+    const [categories, setCategories] = useState<components["schemas"]["Category"][] | undefined>(undefined);
+    const [categoriesLoading, setCategoriesLoading] = useState<boolean>(false);
+    const [categoriesError, setCategoriesError] = useState<undefined | boolean>(undefined);
+
+    const FormSchema = z.object({
+        date: z.date().refine((d) => d <= new Date(), {
+            message: "Datum muss heute sein oder in der Vergangenheit liegen",
+        }),
+        runtime: z.object({
+            hours: z.number().int().nonnegative().max(23),
+            minutes: z.number().int().nonnegative().max(59),
+            seconds: z.number().int().nonnegative().max(59),
+            milliseconds: z.number().int().nonnegative().max(999),
+        }).refine(runtime => {
+            return 0 < runtime.hours + runtime.minutes + runtime.seconds + runtime.milliseconds;
+        }, {
+            message: "Runtime muss größer als 0 sein",
+        }),
+        game: z.string().refine(game => games?.some(g => g.slug === game) ?? false, {}),
+        category: z.string().refine(category => categories?.some(c => c.categoryId === category) ?? false, {}),
+    })
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            date: new Date(),
+            runtime: {
+                hours: 0,
+                minutes: 1,
+                seconds: 30,
+                milliseconds: 0
+            },
+            game: searchParams.get("game") ?? undefined,
+            category: searchParams.get("category") ?? ""
+        },
+    });
+
+    const watchedGame = form.watch("game");
+    const watchedCategory = form.watch("category");
+    const watchedDate = form.watch("date");
+    const watchedRuntime = form.watch("runtime")
+
+    useEffect(() => {
+        async function loadGames(): Promise<void> {
+            const {data, error} = await GetGames();
+
+            setGames(data);
+            setGamesError(error);
+            setGamesLoading(false);
+            if (error != undefined) {
+                toast.error("Es ist ein Fehler beim Laden der Spiele aufgetreten.")
+            }
+        }
+
+        if (!gamesLoading && games == undefined && gamesError == undefined) {
+            loadGames();
+        }
+    }, [])
+
+    useEffect(() => {
+        async function loadCategories(): Promise<void> {
+            const {data, error} = await GetCategoriesFromGame(form.getValues("game"));
+
+            setCategories(data);
+            setCategoriesError(error);
+            setCategoriesLoading(false);
+            if (error != undefined) {
+                toast.error("Es ist ein Fehler beim Laden der Kategorien aufgetreten.")
+            }
+        }
+
+        if (!categoriesLoading && categories == undefined && categoriesError == undefined && form.getValues("game")) {
+            loadCategories();
+        }
+    }, [categories, watchedGame])
+
+    useEffect(() => {
+        setCategories(undefined);
+        setCategoriesError(undefined);
+        setCategoriesLoading(false);
+    }, [watchedGame])
+
+    async function onSubmit(data: z.infer<typeof FormSchema>) {
+        console.log(data);
+    }
+
+
+    return (
+        <Card className="max-w-[400px] mx-auto">
+            <CardHeader>
+                <h1 className="text-2xl font-semibold text-center">Reiche einen Speedrun ein</h1>
+                <CardDescription className="text-center">
+                    Gib die Daten deines Speedruns ein und erscheine in kurzer Zeit auf dem Leaderboard.
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+                        <FormFieldGame form={form} games={games}/>
+                        <FormFieldCategory form={form} categories={categories}/>
+                        <FormFieldDate form={form} watchedCategory={watchedCategory} watchedGame={watchedGame}/>
+                        <FormFieldRuntime form={form} watchedDate={watchedDate} watchedCategory={watchedCategory}
+                                          watchedGame={watchedGame}/>
+                        <Button type="submit" className="w-full cursor-pointer"
+                                disabled={!(watchedCategory && watchedGame && watchedDate && watchedRuntime)}>Absenden</Button>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    );
 }
---
+++ src/presentation/pages/upload-speedrun/components/FormFieldDate.tsx
@@ -0,0 +1,62 @@
+import {FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/presentation/components/ui/form.tsx";
+import {Popover, PopoverContent, PopoverTrigger} from "@/presentation/components/ui/popover.tsx";
+import {Skeleton} from "@/presentation/components/ui/skeleton.tsx";
+import {Button} from "@/presentation/components/ui/button.tsx";
+import {cn} from "@/presentation/lib/utils.ts";
+import {format} from "date-fns";
+import {CalendarIcon} from "lucide-react";
+import {Calendar} from "@/presentation/components/ui/calendar.tsx";
+
+export default function FormFieldDate({form, watchedCategory, watchedGame}: Readonly<{
+    form: any,
+    watchedCategory: string,
+    watchedGame: any
+}>) {
+    return (
+        <FormField
+            control={form.control}
+            name="date"
+            render={({field}) => (
+                <FormItem className="flex flex-col">
+                    <FormLabel>Datum des Speedruns</FormLabel>
+                    <Popover>
+                        {!(watchedCategory && watchedGame) &&
+                            <Skeleton className="h-9 w-full"/>
+                        }
+                        {watchedGame && watchedCategory &&
+                            <PopoverTrigger asChild>
+                                <FormControl>
+                                    <Button
+                                        variant={"outline"}
+                                        className={cn(
+                                            "pl-3 text-left font-normal",
+                                            !field.value && "text-muted-foreground"
+                                        )}
+                                    >
+                                        {field.value ? (
+                                            format(field.value, "d.M.y")
+                                        ) : (
+                                            <span>Pick a date</span>
+                                        )}
+                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50"/>
+                                    </Button>
+                                </FormControl>
+                            </PopoverTrigger>}
+                        <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                    date > new Date() || date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                            />
+                        </PopoverContent>
+                    </Popover>
+                    <FormMessage/>
+                </FormItem>
+            )}
+        />
+    );
+}
