f3a1e0ef49250dc3e74fc21c3abe24c07b5037a8
Lukas Mundt <mail@lukas-mundt.de>
2025-06-02 20:00:01 +0200

    [SE2-39] reduce is-admin api calls

--- src/presentation/hooks/is-admin.ts
+++ src/presentation/hooks/is-admin.ts
@@ -2,14 +2,16 @@ import {useEffect, useState} from "react";
 import {isAdmin as isUserAdmin} from "@/presentation/lib/utils.ts";
 
 export function useIsAdmin() {
-    const [isAdmin, setIsAdmin] = useState<boolean|undefined>(undefined);
+    const [isAdmin, setIsAdmin] = useState<boolean | undefined>(undefined);
 
     useEffect(() => {
-        isUserAdmin().then((isAdmin) => {
-            setIsAdmin(isAdmin);
-        }).catch(() => {
-            setIsAdmin(false);
-        })
+        if (isAdmin == undefined) {
+            isUserAdmin().then((isAdmin) => {
+                setIsAdmin(isAdmin);
+            }).catch(() => {
+                setIsAdmin(false);
+            })
+        }
     })
 
     return isAdmin;
