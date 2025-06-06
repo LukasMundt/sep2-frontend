4303ceb0a893155571a7970b3393f2afd303014a
Lukas Mundt <mail@lukas-mundt.de>
2025-06-07 01:19:45 +0200

    [SE2-37] send token when declining runs

--- src/business-rules/decline-unverified-run.ts
+++ src/business-rules/decline-unverified-run.ts
@@ -1,13 +1,18 @@
 import {components, paths} from "@/data-domain/schema";
 import createClient from "openapi-fetch";
+import Cookies from "universal-cookie";
 
 
 export default async function DeclineUnverifiedRun(
     uuid: components["schemas"]["RunReview"]["run"]["uuid"]
 ): Promise<{ success: boolean, errorMessage?: string }> {
+    const token = new Cookies().get("accessToken");
     const client = createClient<paths>();
     return client.DELETE(
         "/rest/api/reviews/unreviewed/{uuid}", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
             params: {
                 path: {
                     uuid: uuid,
